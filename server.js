30f5c137bca9928a3b875bedeee137026ae2714b
bootcemp1 <nnuriddin8495@gmail.com>
2026-01-03 15:25:26 +0500

    Railway port va server sozlamalari yangilandi

--- server.js
+++ server.js
@@ -14,13 +14,17 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // BAZAGA ULANISH
-mongoose.connect('mongodb://127.0.0.1:27017/uchqorgon_school')
+// BAZAGA ULANISH
+// Agar Railway-da bo'lsa 'process.env.MONGO_URI' ni ishlatadi, 
+// Lokal kompyuterda bo'lsa 'mongodb://127.0.0.1...' ni ishlatadi.
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/uchqorgon_school';
+
+mongoose.connect(MONGO_URI)
   .then(async () => {
       console.log('MongoDB bazaga ulandi!');
       await createDefaultAdmin();
   })
   .catch(err => console.error('Baza xatosi:', err));
-
 // ================= MODELLAR =================
 
 const UserSchema = new mongoose.Schema({
